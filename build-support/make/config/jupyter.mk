8df0760977f27afafd7e279b7b8f46d02b66f213
Cristian Matache <cristianmatache@hotmail.com>
2021-09-11 13:46:16 +0100

    no line len

--- build-support/make/config/jupyter.mk
+++ build-support/make/config/jupyter.mk
@@ -1,3 +1,3 @@
-JUPYTERBLACK_FLAGS=--line-length $(line_len)
+JUPYTERBLACK_FLAGS=--line-length 120
 NBSTRIPOUT_FLAGS=--strip-empty-cells
 FLAKE8_NB_FLAGS=--config build-support/jupyter/tools-config/.flake8_nb
