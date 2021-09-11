2f916607b59526ccb0cc09da7a792d352b1931ea
Cristian Matache <cristianmatache@hotmail.com>
2021-09-11 13:38:39 +0100

    changed reference to repo in comments

--- build-support/make/core/python/type-check.mk
+++ build-support/make/core/python/type-check.mk
@@ -1,5 +1,5 @@
 # !!! Don't edit this file !!!
-# This file is part of AlphaBuild core, don't edit it in a repo other than https://github.com/cristianmatache/workspace
+# This file is part of AlphaBuild core, don't edit it in a repo other than https://github.com/cristianmatache/alpha-build/
 # Please submit an issue/pull request to the main repo if you need any changes in the core infrastructure.
 # Before doing that, you may wish to consider:
 # - updating the config files in build-support/make/config/ to configure tools for your own use case
