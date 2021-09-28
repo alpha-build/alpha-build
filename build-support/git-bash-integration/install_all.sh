1e999078ed3fa6e53f7e1bdb7cf71357b42ab6c9
Cristian Matache <cristianmatache@hotmail.com>
2021-09-28 19:51:53 +0100

    no relative paths

--- build-support/git-bash-integration/install_all.sh
+++ build-support/git-bash-integration/install_all.sh
@@ -2,8 +2,8 @@
 
 CHECKOUT_ROOT=$(realpath "${CHECKOUT_ROOT:-$(dirname "${BASH_SOURCE[0]}")/../..}")
 
-./"$CHECKOUT_ROOT"/build-support/git-bash-integration/install_make.sh
-./"$CHECKOUT_ROOT"/build-support/git-bash-integration/install_zstd.sh
-./"$CHECKOUT_ROOT"/build-support/git-bash-integration/install_libzstd.sh
-./"$CHECKOUT_ROOT"/build-support/git-bash-integration/install_libxxhash.sh
-./"$CHECKOUT_ROOT"/build-support/git-bash-integration/install_rsync.sh
+"$CHECKOUT_ROOT"/build-support/git-bash-integration/install_make.sh
+"$CHECKOUT_ROOT"/build-support/git-bash-integration/install_zstd.sh
+"$CHECKOUT_ROOT"/build-support/git-bash-integration/install_libzstd.sh
+"$CHECKOUT_ROOT"/build-support/git-bash-integration/install_libxxhash.sh
+"$CHECKOUT_ROOT"/build-support/git-bash-integration/install_rsync.sh
