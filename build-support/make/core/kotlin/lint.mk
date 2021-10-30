f0087c784a5085a25a91edf30f2e7d5b9ce9b1dd
George Pereteanu <george.pereteanu@nortonlifelock.com>
2021-10-30 21:53:56 +0300

    Add kotlin linter

---
+++ build-support/make/core/kotlin/lint.mk
@@ -0,0 +1,12 @@
+# !!! Don't edit this file !!!
+# This file is part of AlphaBuild core, don't edit it in a repo other than https://github.com/cristianmatache/alpha-build/
+# Please submit an issue/pull request to the main repo if you need any changes in the core infrastructure.
+# Before doing that, you may wish to consider:
+# - updating the config files in build-support/make/config/ to configure tools for your own use case
+# - writing a new custom rule, in build-support/make/extensions/<lang>/ and import it in the main Makefile
+
+.PHONY: kotlin-lint
+kotlin-lint:
+	$(eval targets := $(onkt))
+	if $(call lang,$(targets),$(REGEX_KT)); then \
+	ktlint $(KT_FORMAT_FLAGS) $(targets); fi;
