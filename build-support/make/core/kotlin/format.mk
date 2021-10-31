1c9b315186cb088c1b86c725760cc3f14e52df58
George Pereteanu <george.pereteanu@nortonlifelock.com>
2021-10-31 11:21:07 +0200

    Rename linters

--- build-support/make/core/kotlin/format.mk
+++ build-support/make/core/kotlin/format.mk
@@ -5,8 +5,8 @@
 # - updating the config files in build-support/make/config/ to configure tools for your own use case
 # - writing a new custom rule, in build-support/make/extensions/<lang>/ and import it in the main Makefile
 
-.PHONY: kotlin-format
-kotlin-format:
+.PHONY: ktlint-format
+ktlint-format:
 	$(eval targets := $(onkt))
 	if $(call lang,$(targets),$(REGEX_KT)); then \
 	find $(targets) -type f -regex $(REGEX_KT) | xargs ktlint -a -F $(KT_FORMAT_FLAGS) $(targets); fi;
