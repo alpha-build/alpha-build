8ddd52ef8f08bf7377fcc6de6ecbc5d7ae0b1fc8
Cristian Matache <cristianmatache@hotmail.com>
2022-04-04 22:44:35 +0100

    version bump

--- build-support/alpha-build/core/setup.py
+++ build-support/alpha-build/core/setup.py
@@ -9,7 +9,7 @@ COMPRESSED_CORE = REPO_ROOT / "alpha_build_core.tar.gz"
 
 setup(
     name="alpha-build-core",
-    version="0.1.2",
+    version="0.1.3",
     description="AlphaBuild's core",
     long_description=README.read_text(),
     long_description_content_type="text/markdown",
